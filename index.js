cf1269da6aa14e5f46d3ad2ac099083b3f8c1f29
WilkyMenezes <willkymenezes@outlook.com>
2022-03-14 20:57:19 -0300

    Aula_ejs

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.post("/", function(req, res){
+    res.send("Post");
+});
+
+app.get("/Pagina", function(req, res){
+    res.send("Pagina 2");
+});
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
