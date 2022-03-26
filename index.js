062a5f6d38a7edaa2d687ed4994206d4f5638197
WilkyMenezes <willkymenezes@outlook.com>
2022-03-26 00:22:58 -0300

    Atualizando novas funções

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const req = require("express/lib/request");
+const { get } = require("express/lib/response");
 const res = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,12 +9,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
+// variavel para receber dados //
+let message = "";
+
+
+
 const pokedex = [
     {
     id: 1,
     nome: "Blastoise",
     tipo: "Water",
     categoia: "Shellfish",
+    detalhes: "",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
 },
 
@@ -22,6 +29,7 @@ const pokedex = [
     nome: "Metapod",
     tipo: "Bug",
     categoia: "Cocoon",
+    detalhes: "",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png"
 },
 {
@@ -29,6 +37,7 @@ const pokedex = [
     nome: "Arbok",
     tipo: "Poison",
     categoia: "Cobra",
+    detalhes: "",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png"
 }
 ]
@@ -39,20 +48,32 @@ const pokedex = [
 // });
 
 app.get("/", (req, res) => {
-    res.render("index", {pokedex});
+    res.render("index", {pokedex,message});
 });
 
 app.get("/cadastro", (req, res) => {
-    res.render("cadastro")
-    
+    res.render("cadastro", {pokedex});
+
+});
+
+app.post("/cadastro", (req, res) => {
+    const pokemon = req.body;
+    pokedex.push(pokemon);
+    message = "Parabéns, seu Pokemon foi cadastrado com sucesso!";
+    setTimeout(() => {
+        message = "";
+      }, 1000);
+
+    res.redirect("/")
+
+});
+
+app.get("/detalhes", (req, res) => {
+    res.render("detalhes", {pokedex});
+
 });
 
 
-app.post("/subscription", (req, res) => {
-    const { nome, email } = req.body;
-    res.send({ nome: nome, email: email });
-    res.send("Formulário Recebido!");
-  });
 
 
 
